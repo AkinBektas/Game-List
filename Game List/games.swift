4310cfcaf9ebf926897d79d540ae7a96b997fda7
Akin Bektas <akin_bektas@hotmail.com>
2015-10-16 13:34:17 +0200

    Deleting to upload a new version

--- Game List/games.swift
+++
@@ -1,9 +0,0 @@
-//
-//  games.swift
-//  Game List
-//
-//  Created by Fhict on 15/10/15.
-//  Copyright (c) 2015 Akin & Michael. All rights reserved.
-//
-
-import Foundation
--- Game List/groep.swift
+++
@@ -1,9 +0,0 @@
-//
-//  groep.swift
-//  Game List
-//
-//  Created by Fhict on 15/10/15.
-//  Copyright (c) 2015 Akin & Michael. All rights reserved.
-//
-
-import Foundation
